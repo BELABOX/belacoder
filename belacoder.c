fe1c84e8b1914acdfb9348297f362a61fddac168
rationalsa <belaboxproject@gmail.com>
2021-02-10 23:18:53 +0000

    Modify the bitrate ctrl parameters for high latency conns

--- belacoder.c
+++ belacoder.c
@@ -34,10 +34,10 @@
 #define ABS_MAX_BITRATE (30 * 1000 * 1000)
 #define DEF_BITRATE (6 * 1000 * 1000)
 #define BITRATE_UPDATE_INT  20 // buffer size sampling interval (ms)
-#define BS_INCR_MIN         12 // maximum buffer size to increase the bitrate when the current bitrate is low
-#define BS4M_INCR           30 // maximum buffer size at 4Mbps to increase the bitrate
-#define BS_DECR_MIN        100 // base value for the scaled BS_DECR (buffer size threshold for reducing the bitrate)
-#define BS4M_DECR           40 // value to add to BS_DECR for 4Mbps (scaled proportionally to the current bitrate)
+#define BS_INCR_MIN         50 // maximum buffer size to increase the bitrate when the current bitrate is low
+#define BS4M_INCR           50 // maximum buffer size at 4Mbps to increase the bitrate
+#define BS_DECR_MIN        200 // base value for the scaled BS_DECR (buffer size threshold for reducing the bitrate)
+#define BS4M_DECR           80 // value to add to BS_DECR for 4Mbps (scaled proportionally to the current bitrate)
 #define BS_DECR_FAST       400 // min buffer size to reduce the bitrate even when the buffer size is already decreasing
 #define BITRATE_INCR_STEP  (100*1000) // the bitrate increment step (bps)
 #define BITRATE_INCR_INT   350        // the minimum interval for increasing the bitrate (ms)
