8f475fc52eccc09c979e3266beda9de43b771efb
rationalsa <belaboxproject@gmail.com>
2021-03-20 20:55:35 +0000

    New bitrate control algorithm combining RTT & send window size
    Implement a new bitrate control algorithm which dynamically measures
    the RTT and send window sizes to adapt to different network conditions

--- belacoder.c
+++ belacoder.c
@@ -33,17 +33,16 @@
 #define MIN_BITRATE (500 * 1000)
 #define ABS_MAX_BITRATE (30 * 1000 * 1000)
 #define DEF_BITRATE (6 * 1000 * 1000)
-#define BITRATE_UPDATE_INT  20 // buffer size sampling interval (ms)
-#define BS_INCR_MIN         12 // maximum buffer size to increase the bitrate when the current bitrate is low
-#define BS4M_INCR           40 // maximum buffer size at 4Mbps to increase the bitrate
-#define BS_DECR_MIN        100 // base value for the scaled BS_DECR (buffer size threshold for reducing the bitrate)
-#define BS4M_DECR           50 // value to add to BS_DECR for 4Mbps (scaled proportionally to the current bitrate)
-#define BS_DECR_FAST       500 // min buffer size to reduce the bitrate even when the buffer size is already decreasing
-#define BITRATE_INCR_STEP  (100*1000) // the bitrate increment step (bps)
-#define BITRATE_INCR_INT   750        // the minimum interval for increasing the bitrate (ms)
-#define BITRATE_DECR_SCALE  10        // the bitrate is decreased by cur_bitrate/BITRATE_DECR_SCALE
-#define BITRATE_DECR_MIN   (100*1000) // the minimum value to decrease the bitrate by (bps)
-#define BITRATE_DECR_INT   500        // the minimum interval for decreasing the bitrate (ms)
+
+#define BITRATE_UPDATE_INT 20
+#define BITRATE_INCR_STEP      (100*1000) // the bitrate increment step (bps)
+#define BITRATE_INCR_INT       300        // the minimum interval for increasing the bitrate (ms)
+
+#define BITRATE_DECR_MIN       (100*1000) // the minimum value to decrease the bitrate by (bps)
+#define BITRATE_DECR_INT       200        // (light congestion) min interval for decreasing the bitrate (ms)
+#define BITRATE_DECR_FAST_INT  250        // (heavy congestion) min interval for decreasing the bitrate (ms)
+#define BITRATE_DECR_SCALE     10         // under heavy congestion, the bitrate is decreased by
+                                          // BITRATE_DECR_MIN + cur_bitrate/BITRATE_DECR_SCALE
 
 // settings ranges
 #define MAX_SOUND_DELAY 10000
@@ -76,6 +75,8 @@ int max_bitrate = DEF_BITRATE;
 int cur_bitrate = MIN_BITRATE;
 
 char *bitrate_filename = NULL;
+
+int srt_latency = DEF_SRT_LATENCY;
 
 uint64_t getms() {
   struct timespec time = {0, 0};
@@ -125,11 +126,7 @@ ret_err:
 }
 
 void update_bitrate() {
-  static double avg_bs = 0;
-  static double prev_bs = 0;
-  static int max_bs = 0;
   static uint64_t next_bitrate_check = 0;
-  static uint64_t next_bitrate_adj = 0;
 
   uint64_t ctime = getms();
   if (ctime < next_bitrate_check) {
@@ -137,55 +134,102 @@ void update_bitrate() {
   }
   next_bitrate_check = ctime + BITRATE_UPDATE_INT;
 
+
+  /*
+   * Send buffer size stats
+   */
   int bs = -1;
   int sz = sizeof(bs);
   int ret = srt_getsockflag(sock, SRTO_SNDDATA, &bs, &sz);
+  assert(ret == 0 && bs >= 0);
 
-  if (ret < 0 || bs < 0) {
-    return;
-  }
-
-  if (bs > max_bs) {
-    max_bs = bs;
-  }
   // Rolling average
-  avg_bs = avg_bs*0.90 + ((double)bs) * 0.10;
+  static double bs_avg = 0;
+  bs_avg = bs_avg*0.99 + (double)bs * 0.01;
 
-  // Scale the buffer size thresholds depending on the current bitrate
-  double scaled_incr_bs = max(BS_INCR_MIN, BS4M_INCR * (double)cur_bitrate/(4*1000*1000));
-  int scaled_bs_decr = BS_DECR_MIN + (int)(BS4M_DECR * (double)cur_bitrate/(4*1000*1000));
-
-  int bitrate = cur_bitrate;
-
-  debug("bs: %d max_bs: %d avg_bs: %f, scaled_target: %f, bitrate %d\n",
-        bs, max_bs, avg_bs, scaled_incr_bs, cur_bitrate);
-
-  if (max_bs < scaled_incr_bs && ctime > next_bitrate_adj) {
-    bitrate += BITRATE_INCR_STEP;
-    next_bitrate_adj = ctime + BITRATE_INCR_INT;
-  } else if (bs > scaled_bs_decr && ctime > next_bitrate_adj && (avg_bs > prev_bs || bs > BS_DECR_FAST)) {
-    bitrate -= max(bitrate/BITRATE_DECR_SCALE, BITRATE_DECR_MIN);
-    next_bitrate_adj = ctime + BITRATE_DECR_INT;
-    /* reset the max bs so we stop decreasing the bitrate
-       as soon as the buffer size goes below the threshold */
-    max_bs = 0;
-  } else {
-    max_bs = max_bs * 95 / 100;
+  // Update the buffer size jitter
+  static double bs_jitter = 0;
+  static int prev_bs = 0;
+  bs_jitter = 0.99 * bs_jitter;
+  int delta_bs = bs - prev_bs;
+  if (delta_bs > bs_jitter) {
+    bs_jitter = (double)delta_bs;
   }
-  prev_bs = avg_bs;
+  prev_bs = bs;
 
-  bitrate = min_max(bitrate, min_bitrate, max_bitrate);
 
-  if (bitrate != cur_bitrate) {
-    cur_bitrate = bitrate;
+  /*
+   * RTT stats
+   */
+  SRT_TRACEBSTATS stats;
+  ret = srt_bstats(sock, &stats, 1);
+  assert(ret == 0);
+  int rtt = (int)stats.msRTT;
 
-    // round the bitrate we set to 100 kbps
-    bitrate = bitrate / (100 * 1000) * (100 * 1000);
-    g_object_set (G_OBJECT(encoder), "bitrate", bitrate / enc_bitrate_div, NULL);
+  // Update the average RTT
+  static double rtt_avg = 0;
+  if (rtt_avg == 0.0) {
+    rtt_avg = (double)rtt;
+  } else {
+    rtt_avg = rtt_avg * 0.99 + 0.01 * (double)rtt;
+  }
 
-    update_overlay(bitrate);
+  // Update the average RTT delta
+  static double rtt_avg_delta = 0;
+  static int prev_rtt = 300;
+  double delta_rtt = (double)(rtt - prev_rtt);
+  rtt_avg_delta = rtt_avg_delta * 0.8 + delta_rtt * 0.2;
+  prev_rtt = rtt;
 
-    debug("set bitrate to %d, internal value %d\n", bitrate, cur_bitrate);
+  // Update the minimum RTT
+  static double rtt_min = 200.0;
+  rtt_min *= 1.001;
+  if (rtt != 100 && rtt < rtt_min && rtt_avg_delta < 1.0) {
+    rtt_min = rtt;
+  }
+
+  // Update the RTT jitter
+  static double rtt_jitter = 0;
+  rtt_jitter *= 0.99;
+  if (delta_rtt > rtt_jitter) {
+    rtt_jitter = delta_rtt;
+  }
+
+
+  debug("bs: %d bs_avg: %f, bs_jitter %f, bitrate %d rtt %d, delta rtt %.0f, avg delta %.1f, avg rtt %.1f, rtt_jitter, %.2f, rtt_min %.1f\n",
+        bs, bs_avg, bs_jitter, cur_bitrate, rtt, delta_rtt, rtt_avg_delta, rtt_avg, rtt_jitter, rtt_min);
+
+
+  static uint64_t next_bitrate_adj = 0;
+  if (ctime > next_bitrate_adj) {
+    int bitrate = cur_bitrate;
+
+    if (rtt < (int)(rtt_min + rtt_jitter*2) && rtt_avg_delta < 0.0) {
+      bitrate += BITRATE_INCR_STEP;
+      next_bitrate_adj = ctime + BITRATE_INCR_INT;
+
+    } else if (rtt > (srt_latency / 5) || bs > (bs_avg + bs_jitter*2.0)) {
+      bitrate -= BITRATE_DECR_MIN + bitrate/BITRATE_DECR_SCALE;
+      next_bitrate_adj = ctime + BITRATE_DECR_FAST_INT;
+
+    } else if (rtt > (int)(rtt_avg + rtt_jitter*5)) {
+      bitrate -= BITRATE_DECR_MIN;
+      next_bitrate_adj = ctime + BITRATE_DECR_INT;
+    }
+
+    bitrate = min_max(bitrate, min_bitrate, max_bitrate);
+
+    if (bitrate != cur_bitrate) {
+      cur_bitrate = bitrate;
+
+      // round the bitrate we set to 100 kbps
+      bitrate = bitrate / (100 * 1000) * (100 * 1000);
+      g_object_set (G_OBJECT(encoder), "bitrate", bitrate / enc_bitrate_div, NULL);
+
+      update_overlay(bitrate);
+
+      debug("set bitrate to %d, internal value %d\n", bitrate, cur_bitrate);
+    }
   }
 }
 
@@ -252,7 +296,7 @@ int parse_ip_port(struct sockaddr_in *addr, char *ip_str, char *port_str) {
   return 0;
 }
 
-int init_srt(char *host, char *port, int srt_latency, char *stream_id) {
+int init_srt(char *host, char *port, char *stream_id) {
   struct addrinfo hints;
   struct addrinfo *addrs;
   memset(&hints, 0, sizeof(hints));
@@ -339,7 +383,7 @@ void cb_pipeline (GstBus *bus, GstMessage *message, gpointer user_data) {
 int main(int argc, char** argv) {
   int opt;
   char *stream_id = NULL;
-  int srt_latency = DEF_SRT_LATENCY;
+  srt_latency = DEF_SRT_LATENCY;
 
   while ((opt = getopt(argc, argv, "d:b:s:l:")) != -1) {
     switch (opt) {
@@ -403,7 +447,7 @@ int main(int argc, char** argv) {
   GstElement *rtlasink = gst_bin_get_by_name(GST_BIN(gst_pipeline), "appsink");
   if (GST_IS_ELEMENT(rtlasink)) {
     gst_app_sink_set_callbacks (GST_APP_SINK(rtlasink), &callbacks, NULL, NULL);
-    int ret = init_srt(argv[optind+1], argv[optind+2], srt_latency, stream_id);
+    int ret = init_srt(argv[optind+1], argv[optind+2], stream_id);
     assert(ret == 0);
 
     len = sizeof(srt_latency);
