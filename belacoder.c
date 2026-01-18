4ef1924a35cdd03e81f36ea18b86d387e8460f8e
rationalsa <belaboxproject@gmail.com>
2026-01-18 14:41:30 +0000

    Update copyright year

--- belacoder.c
+++ belacoder.c
@@ -1,6 +1,6 @@
 /*
     belacoder - live video encoder with dynamic bitrate control
-    Copyright (C) 2020 BELABOX project
+    Copyright (C) 2020-2026 BELABOX project
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
